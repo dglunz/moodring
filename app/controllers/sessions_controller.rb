bc0a8430a95070ddb01f523c34859702e8387b8e
Daniel Glunz <dannyglunz@gmail.com>
2015-01-14 17:23:18 -0700

    clean sessions controller duplication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,17 +15,6 @@ class SessionsController < ApplicationController
     end
   end
 
-  def create
-    user = User.find_by(email: params[:email]).try(:authenticate, params[:password])
-
-    if user
-      session[:user_id] = user.id
-      redirect_to bench_path, notice: "Welcome to Mood Ring"
-    else
-      redirect_to root_path, notice: "We could not log you in. Please try again."
-    end
-  end
-
   def destroy
     session.clear
     redirect_to root_path, notice: "You've been logged out."
